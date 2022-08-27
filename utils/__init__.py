ed9721d51f997907ccef4c09ee67d4025c6adb99
yvonnetsang21 <94978683+yvonnetsang21@users.noreply.github.com>
2022-08-27 17:58:08 +0800

    fix warning bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -23,7 +23,7 @@ def safe_list_access(arr, i):
         return arr[arr_len - 1]
 
 def tgt_angle(tgt):
-    angle, _ = ghcomp.Angle(tgt, ghcomp.UnitX())
+    angle, _ = ghcomp.Angle(tgt, ghcomp.UnitX(1))
     _, y, _ = ghcomp.Deconstruct(tgt)
     larger_pattern, _ = ghcomp.LargerThan(y,0)
     factor = ghcomp.Subtraction(ghcomp.Multiplication(larger_pattern,2),1)
